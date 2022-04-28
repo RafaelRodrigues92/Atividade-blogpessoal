668092cca9e5a023c4639c7066dc0606cb37290d
Rafael <rdiasrodrigues@hotmail.com>
2022-04-28 13:07:05 -0300

    Atividade Blog Pessoal

---
+++ db/db_blog_pessoal.sql
@@ -0,0 +1,8 @@
+use db_blogpessoal;
+
+insert into tb_postagens(data, texto, titulo) values (current_timestamp(), "Blog Pessoal funcionando!", "Minha primeira postagem");
+insert into tb_postagens(data, texto, titulo) values (current_timestamp(), "Blog Pessoal Consultando!", "Minha segunda postagem");
+
+select * from tb_postagens;
+
+delete from tb_postagens where id=4;
