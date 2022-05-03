fefc7f2ce2c691b4c88245d99a4c12e310d4515a
Rafael <rdiasrodrigues@hotmail.com>
2022-05-03 09:04:56 -0300

    Novo

--- src/main/java/com/generation/blogpessoal/model/Tema.java
+++ src/main/java/com/generation/blogpessoal/model/Tema.java
@@ -16,13 +16,12 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 @Entity
 @Table(name = "tb_temas")
 public class Tema{
+	@Id	
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-	    @Id	
-		@GeneratedValue(strategy = GenerationType.IDENTITY)
-		private Long id;
-
-		@NotBlank(message = "O atributo Descrição é obrigatório e não pode conter espaços em branco")
-		private String descricao;
+	@NotBlank(message = "O atributo Descrição é obrigatório e não pode conter espaços em branco")
+	private String descricao;
 
 		/**
 		 *  A Anotação @OneToMany(mappedBy = "tema", cascade = CascadeType.ALL): indica 
@@ -45,34 +44,34 @@ public class Tema{
 		 *  Não esqueça de criar os métodos getters e setters para a Collection postagem.
 		 * 
 		 */
-		@OneToMany(mappedBy = "tema", cascade = CascadeType.REMOVE)
-		@JsonIgnoreProperties("tema")
-		private List<Postagem> postagem;
+	@OneToMany(mappedBy = "tema", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("tema")
+	private List<Postagem> postagem;
 
-		public Long getId() {
-			return id;
-		}
+	public Long getId() {
+		return id;
+	}
 
-		public void setId(Long id) {
-			this.id = id;
-		}
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-		public String getDescricao() {
-			return descricao;
-		}
+	public String getDescricao() {
+		return descricao;
+	}
 
-		public void setDescricao(String descricao) {
-			this.descricao = descricao;
-		}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
 
 		/**
 		 *  Métodos Get e Set para a Collection postagem
 		 */
-		public List<Postagem> getPostagem() {
-			return postagem;
-		}
+	public List<Postagem> getPostagem() {
+		return postagem;
+	}
 
-		public void setPostagem(List<Postagem> postagem) {
-			this.postagem = postagem;
-		}
+	public void setPostagem(List<Postagem> postagem) {
+		this.postagem = postagem;
+	}
 }
